71d0f7d9daae948dec6439a75ad3a20bb09f512c
Huongne1001 <huongne1001@gmail.com>
2024-10-16 00:02:38 +0700

    xu ly logic

--- Ex04/app/src/main/java/com/example/ex04/MainActivity.java
+++ Ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ex04;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,9 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends Activity {
-    EditText edtdoC,edtdoF;
-    Button btncf,btnfc;
+    EditText edtdoC, edtdoF;
+    Button btncf, btnfc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,21 +30,21 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 // TODO Auto-generated method stub
-                DecimalFormat dcf=new DecimalFormat("#.00");
-                String doC = edtdoC.getText()+"";
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doC = edtdoC.getText() + "";
 //
-                int C=Integer.parseInt(doC);
-                edtdoF.setText(""+dcf.format(C*1.8+32));
+                int C = Integer.parseInt(doC);
+                edtdoF.setText("" + dcf.format(C * 1.8 + 32));
             }
         });
         btnfc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DecimalFormat dcf=new DecimalFormat("#.00");
+                DecimalFormat dcf = new DecimalFormat("#.00");
 // TODO Auto-generated method stub
-                String doF = edtdoF.getText()+"";
-                int F=Integer.parseInt(doF);
-                edtdoC.setText(""+dcf.format((F-32)/1.8));
+                String doF = edtdoF.getText() + "";
+                int F = Integer.parseInt(doF);
+                edtdoC.setText("" + dcf.format((F - 32) / 1.8));
             }
         });
     }
