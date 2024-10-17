6440302028951fb79b9b706d973d5e07e332b557
Huongne1001 <huongne1001@gmail.com>
2024-10-17 18:28:13 +0700

    giao dien them moi sinh vien

--- BTH03/app/src/main/java/com/example/bth03/MainActivity.java
+++ BTH03/app/src/main/java/com/example/bth03/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.bth03;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText etStudentId, etFullName, etBirthDate, etEmail, etGpa;
+    private Spinner spnMajor, spnExpYear, spnAddr;
+    private Button btnSubmit;
+    private RadioGroup radioGroupGender;
+    private List<Student> studentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //Khởi tạo các trường nhập
+        etStudentId = findViewById(R.id.etStudentId);
+        etFullName = findViewById(R.id.etFullName);
+        etBirthDate = findViewById(R.id.etBirthDate);
+        etEmail = findViewById(R.id.etEmail);
+        etGpa = findViewById(R.id.etGpa);
+        spnMajor = findViewById(R.id.spnMajor);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        spnExpYear = findViewById(R.id.spnExpYear);
+        radioGroupGender = findViewById(R.id.radioGroupGender);
+        spnAddr = findViewById(R.id.spnAddr);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
