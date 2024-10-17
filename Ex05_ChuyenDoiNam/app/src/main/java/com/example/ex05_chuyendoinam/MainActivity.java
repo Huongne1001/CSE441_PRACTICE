1455b198785a9a279f00dbfac1344ce384c456ef
Huongne1001 <huongne1001@gmail.com>
2024-10-18 03:02:00 +0700

    xu ly logic chuyen nam duong sang nam am su dung can chi

--- Ex05_ChuyenDoiNam/app/src/main/java/com/example/ex05_chuyendoinam/MainActivity.java
+++ Ex05_ChuyenDoiNam/app/src/main/java/com/example/ex05_chuyendoinam/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex05_chuyendoinam;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,45 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //Khai báo các biến giao diện
+    EditText edtduonglich;
+    Button btnchuyen;
+    TextView txt_amlich;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Anh xa ID
+        edtduonglich = findViewById(R.id.edtduonglich);
+        btnchuyen = findViewById(R.id.btnchuyen);
+        txt_amlich = findViewById(R.id.txt_amlich);
+        btnchuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String can ="", chi ="";
+                int namduong = Integer.parseInt(edtduonglich.getText().toString());
+                switch (namduong%10){
+                    case 0: can = "Canh"; break;
+                    case 1: can = "Tân"; break;
+                    case 2: can = "Nhâm"; break;
+                    case 3: can = "Qúy"; break;
+                    case 4: can = "Gíap"; break;
+                    case 5: can = "Ất"; break;
+                    case 6: can = "Bính"; break;
+                    case 7: can = "Đinh"; break;
+                    case 8: can = "Mậu"; break;
+                    case 9: can = "Kỷ"; break;
+
+
+                }
+
+            }
+        }) ;
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
