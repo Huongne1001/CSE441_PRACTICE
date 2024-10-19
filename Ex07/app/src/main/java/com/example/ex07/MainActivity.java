682b873280ea1cb36403ea3504969ea570276e43
Huongne1001 <huongne1001@gmail.com>
2024-10-20 01:47:47 +0700

    Layout activity child

--- Ex07/app/src/main/java/com/example/ex07/MainActivity.java
+++ Ex07/app/src/main/java/com/example/ex07/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ex07;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+     Button btnopen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
