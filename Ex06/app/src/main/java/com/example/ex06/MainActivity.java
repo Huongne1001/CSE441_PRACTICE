5204985fd3ad6fb396c08d3ba620b3221d2971fb
Huongne1001 <huongne1001@gmail.com>
2024-10-19 17:57:34 +0700

    Kiem tra ten hop le

--- Ex06/app/src/main/java/com/example/ex06/MainActivity.java
+++ Ex06/app/src/main/java/com/example/ex06/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex06;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtten,editCMND,editBosung;
+    CheckBox chkdocbao,chkdocsach,chkdoccode;
+    Button btnsend;
+    RadioGroup group;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtten = findViewById(R.id.edtten);
+        editCMND = findViewById(R.id.edtcmnd);
+        editBosung = findViewById(R.id.edtbosung);
+        chkdocbao = findViewById(R.id.chkdocbao);
+        chkdoccode = findViewById(R.id.chkcode);
+        chkdocsach = findViewById(R.id.chkdocsach);
+        btnsend = findViewById(R.id.btnsend);
+        btnsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+// TODO Auto-generated method stub
+                doShowInformation();
+            }
         });
+    }
+    public void doShowInformation()
+    {
+//Kiểm tra tên hợp lệ
+        String ten = edtten.getText().toString();
+        ten = ten.trim();
+        if(ten.length()<3){
+            edtten.requestFocus();
+            edtten.selectAll();
+            Toast.makeText(this, "Tên phải >= 3 ký tự", Toast.LENGTH_LONG).show();
+            return;
+        }
+
     }
 }
