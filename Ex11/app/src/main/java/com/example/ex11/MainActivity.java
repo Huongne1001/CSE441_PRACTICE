ab8e5b3b298ca7d41eb384fcd129939e4943ac02
Huongne1001 <huongne1001@gmail.com>
2024-10-21 01:40:19 +0700

    code logic

--- Ex11/app/src/main/java/com/example/ex11/MainActivity.java
+++ Ex11/app/src/main/java/com/example/ex11/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.ex11;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,12 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btnopen;
     EditText edtlink;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         btnopen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-// TODO Auto-generated method stub
                 Intent intent = new Intent(Intent.ACTION_VIEW,
                         Uri.parse("https://"+edtlink.getText().toString()));
                 startActivity(intent);
