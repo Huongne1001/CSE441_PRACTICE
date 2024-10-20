dadfc43aef9f85431818846a753bb53aa7293cd5
Huongne1001 <huongne1001@gmail.com>
2024-10-20 23:27:19 +0700

    complete

--- BTTH04/app/src/main/java/com/example/btth04/MainActivity.java
+++ BTTH04/app/src/main/java/com/example/btth04/MainActivity.java
@@ -8,6 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.btth04.entities.Student;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +24,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public static class FirebaseDatabaseHelper {
+        private FirebaseDatabase mdatabase;
+        private DatabaseReference mref;
+
+        //constructor
+        public FirebaseDatabaseHelper() {
+            mdatabase = FirebaseDatabase.getInstance();
+            mref = mdatabase.getReference("students");
+        }
+
+        public void addStudent(Student student){
+
+            //get id node con push
+            String id = mref.push().getKey();
+            student.setId(id);
+            //set value student vao node con
+            mref.child(id).setValue(student);
+        }
+
+        public void updateStudent(String id, Student student){
+            //set value student vao node con co id = id
+            mref.child(id).setValue(student);
+        }
+
+        public void deleteStudent(String id){
+            //xoa node con co id = id
+            mref.child(id).removeValue();
+        }
+
+
+        public DatabaseReference getRef(){
+            return mref;
+        }
+
     }
 }
