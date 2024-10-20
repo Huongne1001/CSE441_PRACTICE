cf21e9e48898bc7306a7e206d245c3a2ee1e8255
Huongne1001 <huongne1001@gmail.com>
2024-10-20 16:28:15 +0700

    code mainactivity va resultactivity

--- Ex07_2/app/src/main/java/com/example/ex07_2/ResultActivity.java
+++ Ex07_2/app/src/main/java/com/example/ex07_2/ResultActivity.java
@@ -1,24 +1,49 @@
 package com.example.ex07_2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
+    TextView txtKQ;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txtKQ = findViewById(R.id.txtKQ);
+        btnBack = findViewById(R.id.btnBack);
+
+        //nhận intent
+        Intent myIntent = getIntent();
+        //lay bundle khoi intent
+        Bundle myBundle = myIntent.getBundleExtra("mypackage");
+        //lay du lieu khỏi bundle
+        int a = myBundle.getInt("soa");
+        int b = myBundle.getInt("sob");
+
+        String nghiem = "";
+        if(a == 0 && b==0){
+            nghiem = "PT vo so nghiem";
+        } else if (a==0 && b != 0) {
+            nghiem = "PT vô nghiệm";
+        }else {
+            nghiem = "Ngiệm PT = "+ (-1.0)*b/a;
+        }
+        txtKQ.setText(nghiem);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
